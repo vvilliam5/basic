19e965551a1524d0cf22f2df169ff6b3addd6124
williamswill <willoymaniak@gmail.com>
2019-11-17 17:48:32 +0100

    Updated with 2 comps - CommentDetails and Approval Card

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import RDOM from 'react-dom';
+import faker from 'faker';
+import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
 const App = () => {
-    return <div>
-        <h1>Hello</h1>
-    </div>
+    return (
+        <div className="ui container">
+            <ApprovalCard>
+                <CommentDetail author="Williams" time="Today at 9:00pm" commeent="Nicec Blog Post!" avatar={faker.image.avatar()} />
+            </ApprovalCard>
+        </div>
+    );
 
 }
 
